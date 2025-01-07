94ac9ca9a7e6a7eb9246d9b55831482918c8ce77
module_18_2 <erjan03iopro@gmail.com>
2025-01-08 01:21:22 +0600

    4th commit

--- MySite/tests.py
+++ MySite/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from datetime import datetime
 
-# Create your tests here.
+from MySite.models import Task, CustomUser
+
+
+class CustomUserModelTest(TestCase):
+    def test_create_user(self):
+        user = CustomUser.objects.create(username='TestUser')
+        self.assertEqual(user.username, 'TestUser')
+
+
+class TaskModelTest(TestCase):
+    def test_create_task(self):
+        user = CustomUser.objects.create(username='TestUser')
+        task = Task.objects.create(customer=user, title='Test title1', description='Description test', price=100, deadline=datetime.now())
+        self.assertEqual(task.title, 'Test title1')
+        self.assertEqual(Task.objects.count(), 1)
+
+
+class
